ca7295e35ba902807476940cd5c7de88fc065aaf
BrightonBTC <ccodling+bbtc@gmail.com>
2022-11-24 17:20:05 +0000

    added gevent

--- bija/app.py
+++ bija/app.py
@@ -1,12 +1,13 @@
 
 from flask_socketio import SocketIO
+from engineio.async_drivers import gevent
 from flask import Flask
 from flask_session import Session
 from sqlalchemy.orm import scoped_session
 import bija.db as db
 from bija.gui import init_gui
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../bija/templates')
 socketio = SocketIO(app)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
