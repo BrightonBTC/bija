ac5af3ed2a5deebadba166feb0947b9b933c0e3d
BrightonBTC <ccodling+bbtc@gmail.com>
2022-12-18 04:47:11 +0000

    startup message

--- bija/app.py
+++ bija/app.py
@@ -16,11 +16,11 @@ app.session = scoped_session(db.DB_SESSION)
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
-
 from bija.routes import *
 
 
 def main():
+    print("Bija is now running at http://localhost:{}".format(args.port))
     socketio.run(app, host="0.0.0.0", port=args.port)
 
 
