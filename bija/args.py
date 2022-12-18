6cc31b04ba158b4070a89368d0bf52272c7b1eee
BrightonBTC <ccodling+bbtc@gmail.com>
2022-12-18 04:46:56 +0000

    tidy

--- bija/args.py
+++ bija/args.py
@@ -1,7 +1,6 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-n", "--noqt", dest="noqt", help="No Qt Window, run in your browser instead", action='store_true')
 parser.add_argument("-p", "--port", dest="port", help="Set the port,  default is 5000", default=5000, type=int)
 parser.add_argument("-db", "--db", dest="db", help="Set the database - eg. {name}.sqlite,  default is bija",
                     default='bija', type=str)
