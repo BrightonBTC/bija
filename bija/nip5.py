4c12b112c53392d36d31003863c1fc391371b48a
BrightonBTC <ccodling+bbtc@gmail.com>
2023-01-30 15:03:39 +0000

    nip05 validation refactor

--- bija/nip5.py
+++ bija/nip5.py
@@ -18,7 +18,12 @@ class Nip5:
         self.nip5 = nip5
         self.name = None
         self.address = None
-        self.pk = self.fetch()
+        self.pk = None
+        self.response = None
+
+        self.fetch()
+        if self.response is not None:
+            self.process()
 
     def is_valid_format(self):
         parts = is_nip05(self.nip5)
@@ -31,7 +36,7 @@ class Nip5:
     def match(self, pk):
         if self.pk is not None and self.pk == pk:
             return True
-        elif is_bech32_key('npub', self.pk):  # we shouldn't need this as it's not valid to use bech32 but some services are currently using it
+        elif self.pk is not None and is_bech32_key('npub', self.pk):  # we shouldn't need this as it's not valid to use bech32 but some services are currently using it
             if bech32_to_hex64('npub', self.pk) == pk:
                 return True
         return False
@@ -44,29 +49,37 @@ class Nip5:
                 response = requests.get(
                     url, params={'name': self.name}, timeout=2, headers={'User-Agent': 'Bija Nostr Client'}
                 )
-                logger.info('response staus: {}'.format(response.status_code))
+                logger.info('response status: {}'.format(response.status_code))
                 if response.status_code == 200:
-                    try:
-                        d = response.json()
-                        logger.info('response.json: {}'.format(d))
-                        logger.info('search name: [{}]'.format(self.name))
-                        if self.name in d['names']:
-                            logger.info('name found: {}'.format(self.name))
-                            return d['names'][self.name]
-                        elif self.name.lower() in d['names']:
-                            logger.info('name found: {}'.format(self.name.lower()))
-                            return d['names'][self.name.lower()]
-                    except ValueError:
-                        return None
-                    except Exception as e:
-                        logging.error(traceback.format_exc())
-                else:
-                    return None
-            except ConnectionError:
-                return None
-            except Exception as e:
-                logging.error(traceback.format_exc())
-                return None
-        else:
-            return None
+                    self.response = response
+            except requests.exceptions.HTTPError as e:
+                logger.error("Http Error: {}".format(e))
+                pass
+            except requests.exceptions.ConnectionError as e:
+                logger.error("Error Connecting:".format(e))
+                pass
+            except requests.exceptions.Timeout as e:
+                logger.error("Timeout Error:".format(e))
+                pass
+            except requests.exceptions.RequestException as e:
+                logger.error("OOps: Something Else".format(e))
+                pass
 
+
+    def process(self):
+        try:
+            d = self.response.json()
+            logger.info('response.json: {}'.format(d))
+            logger.info('search name: [{}]'.format(self.name))
+            if self.name in d['names']:
+                logger.info('name found: {}'.format(self.name))
+                self.pk = d['names'][self.name]
+            elif self.name.lower() in d['names']:
+                logger.info('name found: {}'.format(self.name.lower()))
+                self.pk = d['names'][self.name.lower()]
+        except ValueError:
+            logging.error(traceback.format_exc())
+            pass
+        except Exception:
+            logging.error(traceback.format_exc())
+            pass
